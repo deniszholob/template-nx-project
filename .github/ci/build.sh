33e571357d0ab1cbdc6e201d5037d5c4ada6fdbf
Denis Zholob <dddgamer3x@gmail.com>
2024-05-30 13:52:16 -0600

    Convert To Vite/EsBuild

--- .github/ci/build.sh
+++ .github/ci/build.sh
@@ -11,9 +11,9 @@ pnpm run build:gh-prod
 
 # https://angular.io/guide/deployment#deploy-to-github-pages
 echo ' -- Copy index.html to 404.html'
-cp public/index.html public/404.html
+cp dist/browser/index.html dist/browser/404.html
 
 echo ' -- Build contents'
-ls -al "public"
+ls -al "dist"
 
 echo ' -- Build script finished ...'
