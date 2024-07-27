4027deef5cdc118debd31937fd4f7e1eb1e3f740
deniszholob <dddgamer3x@gmail.com>
2024-07-27 23:23:08 +0000

    Deploying to gh-pages from @ deniszholob/template-nx-project@f98a385d7955158ed23f51af2028074dc81d2017 🚀

--- chunk-HNZVTGGE.js
+++
@@ -1,4 +0,0 @@
-import{Aa as o,Ca as a,W as i,db as r,za as s}from"./chunk-YXHS2BYS.js";var f=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=i({type:e,selectors:[["app-dev-page"]],standalone:!0,features:[a],decls:3,vars:0,template:function(n,c){n&1&&(o(0,`
-Space for testing out code.
-`),s(1,"br"),o(2,`Do not save changes to repo
-`))},dependencies:[r],styles:["[_nghost-%COMP%]{display:contents}"]});let t=e;return t})();export{f as DevPageComponent};
