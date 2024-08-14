f51f0b2ea97f5671b91b2ab6c901ecf91bcb1d7b
Denis Zholob <dddgamer3x@gmail.com>
2024-08-13 23:35:02 -0600

    Tailwind template with dark  mode
    * Add local storage util
    * Add theme toggle service
    * Turn on dark mode selection in config and add button on nav
    * Add basic tailwind classes to global css
    * Update html code to include basic tw styling

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,3 +1,5 @@
 export * from './formatter/formatter.util';
+export * from './local-storage/local-storage.util';
 export * from './ng-template/ng-template-typed.directive';
 export * from './object/object.util';
+export * from './theme-toggle/theme-toggle.service';
