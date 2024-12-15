1920a666f188f45d3d03aa5a5490a31994132454
Denis Zholob <dddgamer3x@gmail.com>
2024-12-14 21:33:56 -0700

    Ng 19 upgrade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,10 @@ import { RouterModule } from '@angular/router';
 import { APP_MODIFIED_DATE } from './app.modified';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styles: [':host{display:contents}'],
-  standalone: true,
-  imports: [CommonModule, RouterModule],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styles: [':host{display:contents}'],
+    imports: [CommonModule, RouterModule]
 })
 export class AppComponent {
   protected readonly APP_MODIFIED_DATE: number = APP_MODIFIED_DATE;
--- src/app/pages/dev-page/dev-page.component.ts
+++ src/app/pages/dev-page/dev-page.component.ts
@@ -2,10 +2,9 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-dev-page',
-  templateUrl: './dev-page.component.html',
-  styles: [':host{display:contents}'],
-  standalone: true,
-  imports: [CommonModule],
+    selector: 'app-dev-page',
+    templateUrl: './dev-page.component.html',
+    styles: [':host{display:contents}'],
+    imports: [CommonModule]
 })
 export class DevPageComponent {}
