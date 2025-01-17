8a29be54815a11f45e07e41f4d167834e1dab9f5
Denis Zholob <dddgamer3x@gmail.com>
2025-01-16 19:43:59 -0700

    New code check workflow
    * New  flow to include lint, format, jest-test, build, deploy, storybook build and storybook test
    * Update Formatting
    * Update storybook build dir
    * Remove old workflows
    * Remove npm req
    * Fix Readme typo

--- src/app/pages/dev-page/dev-page.component.ts
+++ src/app/pages/dev-page/dev-page.component.ts
@@ -2,9 +2,9 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-dev-page',
-    templateUrl: './dev-page.component.html',
-    styles: [':host{display:contents}'],
-    imports: [CommonModule]
+  selector: 'app-dev-page',
+  templateUrl: './dev-page.component.html',
+  styles: [':host{display:contents}'],
+  imports: [CommonModule],
 })
 export class DevPageComponent {}
