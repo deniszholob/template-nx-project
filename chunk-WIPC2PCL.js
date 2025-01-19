a2595db6a1c751e18d6033ef8d4814e6dbbc6100
deniszholob <dddgamer3x@gmail.com>
2025-01-19 22:15:39 +0000

    Deploying to gh-pages from @ deniszholob/template-nx-project@48c3167a144d6fa3d1218082f5775ec81c2a9f8b 🚀

--- chunk-WIPC2PCL.js
+++
@@ -1,4 +0,0 @@
-import{Ea as a,Ga as n,Ia as i,V as o,lb as s}from"./chunk-P4YNKARK.js";var d=(()=>{class e{static{this.\u0275fac=function(t){return new(t||e)}}static{this.\u0275cmp=o({type:e,selectors:[["app-dev-page"]],standalone:!0,features:[i],decls:3,vars:0,template:function(t,r){t&1&&(n(0,`
-Space for testing out code.
-`),a(1,"br"),n(2,`Do not save changes to repo
-`))},dependencies:[s],styles:["[_nghost-%COMP%]{display:contents}"]})}}return e})();export{d as DevPageComponent};
