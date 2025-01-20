8441be1a9b5471fb440692d7788637ed8edfc663
Denis Zholob <dddgamer3x@gmail.com>
2025-01-19 20:17:33 -0700

    Update jest setup

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -1,8 +1,7 @@
-// @ts-expect-error https://thymikee.github.io/jest-preset-angular/docs/getting-started/test-environment
-globalThis.ngJest = {
-  testEnvironmentOptions: {
-    errorOnUnknownElements: true,
-    errorOnUnknownProperties: true,
-  },
-};
-import 'jest-preset-angular/setup-jest';
+// https://thymikee.github.io/jest-preset-angular/docs/getting-started/test-environment/
+import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+
+setupZoneTestEnv({
+  errorOnUnknownElements: true,
+  errorOnUnknownProperties: true,
+});
