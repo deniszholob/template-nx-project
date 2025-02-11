4f9098fde66fc60eab6c7457d808cd17d3bf281f
deniszholob <dddgamer3x@gmail.com>
2025-02-11 15:45:19 +0000

    Deploying to gh-pages from @ deniszholob/template-nx-project@e24fb2a248e587b4e044ac00ec81f35af1d49e47 🚀

--- chunk-5PLVHFBR.js
+++
@@ -1,4 +0,0 @@
-import{Oa as i,Qa as o,kb as p,ya as n}from"./chunk-SB6CQQQL.js";var m=(()=>{class e{static{this.\u0275fac=function(t){return new(t||e)}}static{this.\u0275cmp=n({type:e,selectors:[["app-dev-page"]],decls:3,vars:0,template:function(t,a){t&1&&(o(0,`
-Space for testing out code.
-`),i(1,"br"),o(2,`Do not save changes to repo
-`))},dependencies:[p],styles:["[_nghost-%COMP%]{display:contents}"]})}}return e})();export{m as DevPageComponent};
---
+++ chunk-7JVMMYTL.js
@@ -0,0 +1,4 @@
+import{Na as i,Pa as o,jb as p,xa as n}from"./chunk-RBY5YFLD.js";var m=(()=>{class e{static{this.\u0275fac=function(t){return new(t||e)}}static{this.\u0275cmp=n({type:e,selectors:[["app-dev-page"]],decls:3,vars:0,template:function(t,a){t&1&&(o(0,`
+Space for testing out code.
+`),i(1,"br"),o(2,`Do not save changes to repo
+`))},dependencies:[p],styles:["[_nghost-%COMP%]{display:contents}"]})}}return e})();export{m as DevPageComponent};
